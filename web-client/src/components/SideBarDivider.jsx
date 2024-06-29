53fb70cad881fb628b9fad436ad57906b3bc5515
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-06-29 16:54:26 -0500

    fix: clean up

--- web-client/src/components/SideBarDivider.jsx
+++
@@ -1,7 +0,0 @@
-function SideBarDivider({ text }) {
-  return (
-      <div className="text-slate-400 text-sm mt-5 ml-3 mb-[-8px] hidden md:flex">{ text }</div>
-  );
-}
-
-export default SideBarDivider;
